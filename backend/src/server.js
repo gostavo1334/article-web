b82306e2851611c170b43c3b1d8f3b2219310c2a
gosta vo <dwwww@gos.tux.com>
2025-08-09 14:01:15 +0700

    mmmm

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,64 +1,50 @@
 import express from 'express';
-import { MongoClient } from 'mongodb';
+
+let articleInfo =[{
+    name: 'First-Article',
+    upvoted: 0,
+    comment: [],
+},{
+    name: 'Second-Article',
+    upvoted: 0,
+    comment: [],
+},{
+    name: 'Third-Article',
+    upvoted: 0,
+    comment: [],
+},{
+    name: 'Learn-React',
+    upvoted: 0,
+    comment: [],
+}]
+
+
 const app = express();
 app.use(express.json());
-
-app.get('/api/articles/:name', async (req, res) => {
-const { name } = req.params;
-
-const client = new MongoClient('mongodb://127.0.0.1:27017');
-await client.connect();
-
-const db = client.db('react-blog-db'); // raect-blog-db is the name of the database
-
-const article = await db.collection('articles').findOne({ name });
-
-if (article) {
-  res.json(article);
-}else{
-  res.status(404).send('Article not found');
-
-}
-
-
-
-});
-
-app.put('/api/articles/:name/upvote', async(req, res) => {
-  const{ name } = req.params;
-  //const article = articlesInfo.find(article => article.name === name);
-
-const client = new MongoClient('mongodb://127.0.0.1:27017');
-await client.connect();
-
-const db = client.db('react-blog-db'); // raect-blog-db is the name of the database
-await db.collection('articles').updateOne({ name }, { 
-  $inc: { upvotes: 1 },
-  $set: { upvotes: 100}
- })
-
-
-
-  if (article) {
-    article.upvotes += 1;
-    res.send(`The ${name} article now has ${article.upvotes} upvotes????d??`);
-  } else {
-    res.send('The article doesn\'t exist');
+app.put('/api/articles/:name/upvote', (req, res) => {
+    const { name } = req.params;
+    const article = articleInfo.find(a => a.name === name);
+    if(!article){
+    article.upvoted += 1;
+    res.send(`The article "${name}" now has ${article.upvoted} upvotes.`);
+    }else{
+        res.status('the article does not exist');
     }
-  });
-
+});
 app.post('/api/articles/:name/comment', (req, res) => {
-  const { name } = req.params;
-  const { postedBy, text } = req.body;
-  const article = articlesInfo.find(article => article.name === name);
-  if(article){
-    article.comments.push({ postedBy, text });
-    res.send(article.comments);
-  }else{
-    res.send('The article doesn\'t exist');
-  }
+    const { name } = req.params;
+    const {postBy, text} = req.body;
+
+    const article = articleInfo.find(a => a.name === name);
+    if(article){
+        article.comment.push({ postBy, text })
+        res.send(article.comment);
+    }else{
+        res.send ('that article no exist')
+    }
+        
 });
 
 app.listen(8000, () => {
-  console.log('Server is running on http://localhost:8000/hello');
+    console.log('Server is running on http://localhost:8000');
 });
